79d492ba46230defffcdeb495c5d64a544d2d917
ErrEgEdEcE <rocio.garciadelcorro@unahur.edu.ar>
2026-05-18 00:29:44 -0300

    Agregué rutas, corregí middleware y setée main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
 console.log("UnaHur - Anti-Social net");
+const express = require('express');
+const app = express();
+const sequelize = require('./config/database');
+const postRoutes = require('./routes/postRoutes')
+const PORT = 3000;
+
+app.use(express.json());
+app.use('/api/posts',postRoutes);
+
+//SINCRO CON BASE DE DATOS
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('¡Base de datos SQLite sincronizada correctamente!');
+    
+    // Encendemos el servidor para escuchar conexiones de Postman o React
+    app.listen(PORT, () => {
+      console.log(`==================================================`);
+      console.log(` Servidor corriendo en: http://localhost:${PORT} `);
+      console.log(`==================================================`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error de programación al conectar la base de datos:', error.message);
+  });
---
+++ src/routes/postRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { createPost, getAllPosts, getPostById, updatePost, deletePost, addImageToPost, removeImageFromPost  } = require('../controllers/postController');
+const router = express.Router();
+const {validatePost} = require('../middlewares/postValidation');
+
+router.post('/', validatePost, createPost);
+router.get('/', getAllPosts);
+router.get('/:id', getPostById);
+router.put('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.post('/:id/images', addImageToPost);
+router.delete('images/:imageId', removeImageFromPost);
+
+module.exports = router;
